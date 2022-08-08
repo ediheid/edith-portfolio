e067f657a38601e6801e94971471704621e435a2
Edith-Heidmann <eheidmann@gmail.com>
2022-08-08 13:37:54 +0200

    Created Comntext wrapper and wrapped around App.js

---
+++ Context/AppContext.js
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const AppContext = createContext();
+
+// App Wrapper - (wrapped around App Component)
+export const AppWrapper = ({ children }) => {
+  // Contact form state
+  let [isContactFormOpen, setIsContactFormOpen] = useState(false);
+
+  //  Global Window events
+  useEffect(() => {
+    // Initialise AOS on every render
+    AOS.init();
+
+    // Todo: Seperate nav for web and art
+    // Closes mobile nav dropdown if it is open and the user scrolls down the page past 100px
+    // window.onscroll = function () {
+    //   if (window.scrollY > 100) {
+    //     setIsNavOpen(false);
+    //   }
+    // };
+
+    // Scroll to top on browser refresh
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
+    // ! (Page needs to be rendered before accessing window)
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ isContactFormOpen }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// UseContext Function
+// To be imported into any Component and called where needed for state to be passed in
+// eg. useAppContext().isOpen
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,14 @@
-import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import { AppWrapper } from "../Context/AppContext";
 
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  // Initialising AOS on every render
-  useEffect(() => {
-    AOS.init();
-  }, []);
-
-  return <Component {...pageProps} />;
+  return (
+    // AppWrapper Holds Context
+    <AppWrapper>
+      <Component {...pageProps} />
+    </AppWrapper>
+  );
 }
 
 export default MyApp;
