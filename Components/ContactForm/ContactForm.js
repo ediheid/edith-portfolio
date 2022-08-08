72371637af48e986de9f15ba5fb349e3a8fe5fa3
Edith-Heidmann <eheidmann@gmail.com>
2022-08-08 16:01:51 +0200

    Started styling form

--- Components/ContactForm/ContactForm.js
+++ Components/ContactForm/ContactForm.js
@@ -11,7 +11,32 @@ const ContactForm = () => {
         >
           Close
         </button>
-        <div>This will be the contact form modal</div>
+        <form className={styles["form"]}>
+          <div>
+            <div className={styles["label-input-containers"]}>
+              <label>Name</label>
+              <input type="text"></input>
+            </div>
+
+            <div className={styles["label-input-containers"]}>
+              <label>email</label>
+              <input type="email"></input>
+            </div>
+
+            <div className={styles["label-input-containers"]}>
+              <label>Subject</label>
+              <input type="text"></input>
+            </div>
+          </div>
+          <div>
+            <div className={styles["label-input-containers"]}>
+              <label>Message</label>
+              <textarea className={styles["text-area"]} type="text"></textarea>
+            </div>
+
+            <button className={styles["submit-button"]}>Send</button>
+          </div>
+        </form>
       </div>
     </div>
   );
