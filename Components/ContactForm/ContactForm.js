a214bde28bd3f581142f26877354c5551b774d50
Edith-Heidmann <eheidmann@gmail.com>
2022-08-08 15:34:38 +0200

    Started styling Contact Modal

--- Components/ContactForm/ContactForm.js
+++ Components/ContactForm/ContactForm.js
@@ -1,9 +1,18 @@
+import { useAppContext } from "../../Context/AppContext";
 import styles from "./contact-form.module.scss";
 
 const ContactForm = () => {
   return (
     <div className={styles["contact-form-page-container"]}>
-      <div>This will be the contact form modal</div>
+      <div className={styles["contact-modal"]}>
+        <button
+          className={styles["close-button"]}
+          onClick={useAppContext().closeContactFormModal}
+        >
+          Close
+        </button>
+        <div>This will be the contact form modal</div>
+      </div>
     </div>
   );
 };
