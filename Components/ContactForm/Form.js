77ab7b3e7439ab896a353b503f0e21dd99889e03
Edith-Heidmann <eheidmann@gmail.com>
2022-08-09 14:41:42 +0200

    Added for tags to correlate to each input and styled control groups to highlight label and input on focus

--- Components/ContactForm/Form.js
+++ Components/ContactForm/Form.js
@@ -7,27 +7,52 @@ const Form = () => {
         {/* Name, email and subject */}
         <div>
           <div className={styles["label-input-containers"]}>
-            <label className={styles["labels"]}>Name</label>
-            <input className={styles["input"]} type="text"></input>
+            <label className={styles["labels"]} htmlFor="inputName">
+              Name
+            </label>
+            <input
+              className={styles["input"]}
+              type="text"
+              id="inputName"
+            ></input>
           </div>
 
           <div className={styles["label-input-containers"]}>
-            <label className={styles["labels"]}>email</label>
-            <input className={styles["input"]} type="text"></input>
+            <label className={styles["labels"]} htmlFor="inputEmail">
+              email
+            </label>
+            <input
+              className={styles["input"]}
+              type="text"
+              id="inputEmail"
+            ></input>
           </div>
 
           <div className={styles["label-input-containers"]}>
-            <label className={styles["labels"]}>Subject</label>
-            <input className={styles["input"]} type="text"></input>
+            <label className={styles["labels"]} htmlFor="inputSubject">
+              Subject
+            </label>
+            <input
+              className={styles["input"]}
+              type="text"
+              id="inputSubject"
+            ></input>
           </div>
         </div>
         {/* Message and button */}
         <div>
           <div className={styles["label-input-containers"]}>
-            <label className={styles["labels"]}>Message</label>
-            <textarea className={styles["text-area"]} type="text"></textarea>
+            <label className={styles["labels"]} htmlFor="inputMessage">
+              Message
+            </label>
+            <textarea
+              className={styles["text-area"]}
+              type="text"
+              id="inputMessage"
+            ></textarea>
           </div>
-
+        </div>
+        <div className={styles["button-wrapper"]}>
           <button className={styles["submit-button"]}>Send</button>
         </div>
       </form>
