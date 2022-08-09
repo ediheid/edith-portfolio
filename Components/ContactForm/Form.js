f6ccdefd2adca068f7917849e59b4d7c0eb0f590
Edith-Heidmann <eheidmann@gmail.com>
2022-08-09 13:50:08 +0200

    Testing input font size

--- Components/ContactForm/Form.js
+++ Components/ContactForm/Form.js
@@ -7,24 +7,24 @@ const Form = () => {
         {/* Name, email and subject */}
         <div>
           <div className={styles["label-input-containers"]}>
-            <label>Name</label>
-            <input type="text"></input>
+            <label className={styles["labels"]}>Name</label>
+            <input className={styles["input"]} type="text"></input>
           </div>
 
           <div className={styles["label-input-containers"]}>
-            <label>email</label>
-            <input type="text"></input>
+            <label className={styles["labels"]}>email</label>
+            <input className={styles["input"]} type="text"></input>
           </div>
 
           <div className={styles["label-input-containers"]}>
-            <label>Subject</label>
-            <input type="text"></input>
+            <label className={styles["labels"]}>Subject</label>
+            <input className={styles["input"]} type="text"></input>
           </div>
         </div>
         {/* Message and button */}
         <div>
           <div className={styles["label-input-containers"]}>
-            <label>Message</label>
+            <label className={styles["labels"]}>Message</label>
             <textarea className={styles["text-area"]} type="text"></textarea>
           </div>
 
