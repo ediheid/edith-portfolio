412033052afbc9dff338226759bca9ac225cec08
Edith-Heidmann <eheidmann@gmail.com>
2022-09-07 15:09:49 +0200

    Starterd migrating to TypeScript - added all config files and updated app.js to ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
+import type { AppProps } from "next/app";
 import { AppWrapper } from "../Context/AppContext";
 
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     // AppWrapper Holds Context
     <AppWrapper>
