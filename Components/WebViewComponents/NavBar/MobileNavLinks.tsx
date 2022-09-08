40745650db3df6b5192e2efad2491f793d69fa2a
ediheid <eheidmann@gmail.com>
2022-09-08 19:47:01 +0200

    Added contact form to Mobile nav links with onClick event

--- Components/WebViewComponents/NavBar/MobileNavLinks.tsx
+++ Components/WebViewComponents/NavBar/MobileNavLinks.tsx
@@ -1,17 +1,29 @@
+import { useGlobalContext } from "../../../Context";
+import ContactForm from "../../ContactForm/ContactForm";
 import styles from "./mobile-nav-bar.module.scss";
 
 const MobileNavLinks = () => {
+  const { contactOpen } = useGlobalContext();
+  const { setContactOpen } = useGlobalContext();
+
+  const openContactFormModal = () => {
+    setContactOpen(true);
+  };
+
   return (
-    <div className={styles["nav-dropdown-container"]}>
-      <ul className={styles["nav-list"]}>
-        <li>About</li>
-        <li>Websites</li>
-        <li>Web Design</li>
-        <li>Web Apps</li>
-        <li>Home</li>
-        <li>Contact</li>
-      </ul>
-    </div>
+    <>
+      <div className={styles["nav-dropdown-container"]}>
+        <ul className={styles["nav-list"]}>
+          <li>About</li>
+          <li>Websites</li>
+          <li>Web Design</li>
+          <li>Web Apps</li>
+          <li>Home</li>
+          <li onClick={openContactFormModal}>Contact</li>
+        </ul>
+      </div>
+      {contactOpen && <ContactForm />}
+    </>
   );
 };
 
