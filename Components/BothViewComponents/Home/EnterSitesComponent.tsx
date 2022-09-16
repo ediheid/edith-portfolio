2f267e59fbb39fb4e0a54d57af25f429f4903ffb
Edith-Heidmann <eheidmann@gmail.com>
2022-09-16 17:05:01 +0200

    Started bulding enter site image component

--- Components/BothViewComponents/Home/EnterSitesComponent.tsx
+++ Components/BothViewComponents/Home/EnterSitesComponent.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import styles from "./enter-split-sites.module.scss";
+import Image from "next/image";
+import slide1 from "./static/slide-1.png";
 
 const EnterSitesComponent = ({ site }: { [key: string]: any }) => {
   return (
