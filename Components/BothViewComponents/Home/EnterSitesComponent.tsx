4dd335f41177653ca6ae46fb89dda3c8725559c6
Edith-Heidmann <eheidmann@gmail.com>
2022-09-16 17:54:37 +0200

    Adding latest changes with background image ID commented out

--- Components/BothViewComponents/Home/EnterSitesComponent.tsx
+++ Components/BothViewComponents/Home/EnterSitesComponent.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import styles from "./enter-split-sites.module.scss";
-import Image from "next/image";
-import slide1 from "./static/slide-1.png";
 
 const EnterSitesComponent = ({ site }: { [key: string]: any }) => {
   return (
     <>
       <Link href={site.href}>
-        <div className={styles["content-container"]}></div>
+        <div
+          className={styles["content-container"]}
+          // id={styles[`${site.imageId}`]}
+        ></div>
       </Link>
     </>
   );
--- Components/BothViewComponents/Home/enterSiteData.ts
+++ Components/BothViewComponents/Home/enterSiteData.ts
@@ -1,4 +1,4 @@
-export const enterSiteData: { id: string; href: string }[] = [
-  { id: "web", href: "/web" },
-  { id: "art", href: "/art" },
+export const enterSiteData: { id: string; href: string; imageId: string }[] = [
+  { id: "web", href: "/web", imageId: "web-bg" },
+  { id: "art", href: "/art", imageId: "art-bg" },
 ];
