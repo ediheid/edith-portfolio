d2e6dd40e8e1e38075f40b42183e99fab6d68fd6
ediheid <eheidmann@gmail.com>
2022-09-21 14:01:45 +0200

    Uncommented Reverberation component to fix build

--- pages/web/[reverberation].tsx
+++ pages/web/[reverberation].tsx
@@ -1,18 +1,18 @@
 // Custom Next hook for dynamic routing
 import { useRouter } from "next/router";
 
-// const Reverberation = () => {
-//   const router = useRouter();
-//   // complete url for dynamic route.
-//   router.query.reverberation;
+const Reverberation = () => {
+  const router = useRouter();
+  // complete url for dynamic route.
+  router.query.reverberation;
 
-//   return (
-//     <div>
-//       This will be a Layout Component for all individual project pages that will
-//       the hold the project component..
-//       <div> This will be the project component</div>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      This will be a Layout Component for all individual project pages that will
+      the hold the project component..
+      <div> This will be the project component</div>
+    </div>
+  );
+};
 
-// export default Reverberation;
+export default Reverberation;
