23c986c54e39ef3f4cae5f897d28cd510c9e4d85
ediheid <eheidmann@gmail.com>
2022-09-21 17:54:11 +0200

    Updated nav and logo link font, colour and sizing

--- Components/PagesWeb/LogoLink/LogoLink.tsx
+++ Components/PagesWeb/LogoLink/LogoLink.tsx
@@ -2,23 +2,19 @@ import Link from "next/link";
 import styles from "./logo-link.module.scss";
 
 type LogoProps = {
-  initials: string
-  extraText: string
-}
+  initials: string;
+};
 
 let logoData = {
-initials: "eh",
-extraText: " .home"
+  initials: "eh.",
+};
 
-}
-
-const LogoLink = ({} : LogoProps) => {
+const LogoLink = ({}: LogoProps) => {
   return (
     <div className={styles["home-link-container"]}>
       <Link href="/">
         <div>
           <span>{logoData.initials}</span>
-          <span>{logoData.extraText}</span>
         </div>
       </Link>
     </div>
