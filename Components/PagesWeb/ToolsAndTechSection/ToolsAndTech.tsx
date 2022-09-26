fc7db53629cffc1e3ec657e61cb0fe84ef5eecfd
Edith-Heidmann <eheidmann@gmail.com>
2022-09-26 17:49:31 +0200

    Added structure for logo animation

--- Components/PagesWeb/ToolsAndTechSection/ToolsAndTech.tsx
+++ Components/PagesWeb/ToolsAndTechSection/ToolsAndTech.tsx
@@ -1,5 +1,35 @@
+import styles from "./tools-and-tech.module.scss";
+
+const icons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
 const ToolsAndTech = () => {
-  return <>This will be the tools and tech section</>;
+  return (
+    <section className={styles["tools-tech-container"]}>
+      <div className={styles["animation-container"]}>
+        <div className={`${styles["animation-element"]} ${styles["primary"]}`}>
+          {icons.map((icon, index) => {
+            return (
+              <div className={styles["icon"]} key={index}>
+                {icon}
+              </div>
+            );
+          })}
+        </div>
+
+        <div
+          className={`${styles["animation-element"]} ${styles["secondary"]}`}
+        >
+          {icons.map((icon, index) => {
+            return (
+              <div className={styles["icon"]} key={index}>
+                {icon}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ToolsAndTech;
