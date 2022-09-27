c495b6efcfe8de65c4975b1a78686201974d3787
ediheid <eheidmann@gmail.com>
2022-09-27 16:45:28 +0200

    Added icons for animation - need to style for mobile

--- Components/PagesWeb/ToolsAndTechSection/ToolsAndTech.tsx
+++ Components/PagesWeb/ToolsAndTechSection/ToolsAndTech.tsx
@@ -1,6 +1,49 @@
+/* eslint-disable react/jsx-key */
+import { SiJavascript } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { SiReact } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaVuejs } from "react-icons/fa";
+import { FaSass } from "react-icons/fa";
+import { SiCss3 } from "react-icons/si";
+import { SiHtml5 } from "react-icons/si";
+import { FaFigma } from "react-icons/fa";
+import { SiAdobephotoshop } from "react-icons/si";
+import { SiNodedotjs } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { FaNpm } from "react-icons/fa";
+import { SiPostman } from "react-icons/si";
+import { FaGitAlt } from "react-icons/fa";
+import { SiGithub } from "react-icons/si";
+import { SiGitlab } from "react-icons/si";
+
 import styles from "./tools-and-tech.module.scss";
 
-const icons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const icons = [
+  // eslint-disable-next-line react/jsx-key
+  <SiJavascript />,
+  <SiTypescript />,
+  <SiReact />,
+  <TbBrandNextjs />,
+  <FaVuejs />,
+  <FaSass />,
+  <SiCss3 />,
+  <SiHtml5 />,
+  <FaFigma />,
+];
+
+const icons2 = [
+  <SiAdobephotoshop />,
+  <SiNodedotjs />,
+  <SiExpress />,
+  <SiMongodb />,
+  <FaNpm/>,
+  <SiPostman/>,
+  <FaGitAlt/>,
+  <SiGithub/>,
+  <SiGitlab/>
+];
 
 const ToolsAndTech = () => {
   return (
@@ -20,6 +63,33 @@ const ToolsAndTech = () => {
           className={`${styles["animation-element"]} ${styles["secondary"]}`}
         >
           {icons.map((icon, index) => {
+            return (
+              <div className={styles["icon"]} key={index}>
+                {icon}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+
+       {/*  Other direction */}
+
+       <div className={styles["animation-container"]}>
+        <div className={`${styles["animation-element"]} ${styles["primary2"]}`}>
+          {icons2.map((icon, index) => {
+            return (
+              <div className={styles["icon"]} key={index}>
+                {icon}
+              </div>
+            );
+          })}
+        </div>
+
+        <div
+          className={`${styles["animation-element"]} ${styles["secondary2"]}`}
+        >
+          {icons2.map((icon, index) => {
             return (
               <div className={styles["icon"]} key={index}>
                 {icon}
